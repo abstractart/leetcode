a78f57d36a50093021ce90bd28bf8c9867c2d64d
Eugene Kozlov <kozlovea8@gmail.com>
2020-10-13 13:34:51 +0300

    Create my-using-sort.py

---
+++ easy-collection/217-contains-duplicate/my-using-sort.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        nums.sort()
+        
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]: return True
+            
+        return False
+        
+        
+  
+  
