e7b7f21c80754d61abd364843b5d226288f22734
Eugene Kozlov <kozlovea8@gmail.com>
2020-10-14 08:47:33 +0300

    Update my-recursive.py

--- 00206/reverse-linked-list/my-recursive.py
+++ 00206/reverse-linked-list/my-recursive.py
@@ -13,3 +13,20 @@ class Solution:
         
         return self.reverseListRec(source, target)
         
+
+        
+   class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        return self.reverseListRec(head, None)
+    
+    def reverseListRec(self, source, target):
+        if not source: return target
+        
+        # New Head of source list
+        newSource = source.next
+        
+        # head of target list
+        source.next = target
+        target = source
+        
+        return self.reverseListRec(newSource, target)
