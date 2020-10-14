014d13878af1f1d9f243f41e0542832aa8de8fef
Eugene Kozlov <kozlovea8@gmail.com>
2020-10-14 09:14:00 +0300

    Update my-recurive.py

--- 00101-symmetric-tree/my-recurive.py
+++ 00101-symmetric-tree/my-recurive.py
@@ -2,15 +2,13 @@ class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
         if not root: return True
         
-        return self.isSymmetricTrees(root.left, root.right)
+        return self.helper(root.left, root.right)
         
 
-    def isSymmetricTrees(self, left, right):
+    def helper(self, left, right):
         if not left and not right: return True
         
-        if left and right:
-            if left.val != right.val: return False 
-            
-            return self.isSymmetricTrees(left.left, right.right) and self.isSymmetricTrees(left.right, right.left) 
+        if left and right and left.val == right.val:
+            return self.helper(left.left, right.right) and self.helper(right.left, left.right)
         
         return False
