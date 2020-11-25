33380dd701b64a7fbc3405d49fe8d1bad9de9142
Eugene Kozlov <kozlovea8@gmail.com>
2020-11-25 19:27:57 +0300

    Create my-optimized-with-inplace-merge.py

---
+++ 00023-merge-k-sorted-lists/my-optimized-with-inplace-merge.py
@@ -0,0 +1,29 @@
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if len(lists) == 0: return None
+        
+        while(len(lists) > 1):                    
+            lists.insert(0, (self.merge(lists.pop(), lists.pop())))
+            
+        return lists[0]
+    
+    
+    def merge(self, l1, l2):
+        curr1, curr2 = l1, l2
+        head = ListNode(0)
+        tail = head
+        
+        while(curr1 and curr2):
+            if curr1.val < curr2.val:
+                tail.next = curr1
+                curr1 = curr1.next
+            else:
+                tail.next = curr2
+                curr2 = curr2.next
+            
+            tail = tail.next
+            tail.next = None
+            
+        tail.next = curr1 or curr2
+        
+        return head.next
