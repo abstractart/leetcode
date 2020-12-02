aee276475679ef0fb8236f560f207c8bc574b6bb
Eugene Kozlov <kozlovea8@gmail.com>
2020-12-02 10:05:12 +0300

    Update my-recursive.py

--- 00108-convert-sorted-array-to-binary-search-tree/my-recursive.py
+++ 00108-convert-sorted-array-to-binary-search-tree/my-recursive.py
@@ -1,19 +1,16 @@
 class Solution:
-    def sortedArrayToBST(self, nums):
-        return self.recursive(nums, 0, len(nums) - 1)
-        
-        
-    def recursive(self, nums, start, finish):
-        if start > finish: return
-        
-        if start == finish: return TreeNode(nums[start])
-        
-        middle = (finish + start) // 2
-        
-        root = TreeNode(nums[middle])
-        
-        root.left = self.recursive(nums, start, middle - 1)
-        root.right = self.recursive(nums, middle + 1, finish)
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        return self.helper(nums, 0, len(nums) - 1)
     
-        return root
+    
+    def helper(self, nums, start, finish):
+        if start > finish: return None
         
+        middle = start + (finish - start) // 2
+        
+        node = ListNode(nums[middle])
+        
+        node.left = self.helper(nums, start, middle - 1)
+        node.right = self.helper(nums, middle + 1, finish)
+        
+        return node
