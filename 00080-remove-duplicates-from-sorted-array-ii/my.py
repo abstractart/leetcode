0a1828d528f76650f9bb12dde14ac8fe67edd5ee
Eugene Kozlov <kozlovea8@gmail.com>
2020-12-14 10:53:21 +0300

    Update my.py

--- 00080-remove-duplicates-from-sorted-array-ii/my.py
+++ 00080-remove-duplicates-from-sorted-array-ii/my.py
@@ -4,8 +4,7 @@ class Solution:
 
         l = 2
         for i in range(2, len(nums)):    
-            if nums[i] == nums[l - 1]:
-                if nums[l - 1] == nums[l - 2]: continue
+            if nums[i] == nums[l - 1] and nums[l - 1] == nums[l - 2]: continue
 
             nums[l] = nums[i]
             l += 1
