8e76caf60d654a71fd264d753d725cf359e7158e
Eugene Kozlov <kozlovea8@gmail.com>
2021-02-02 21:35:55 +0300

    Create my.py

---
+++ 01512-number-of-good-pairs/01470-shuffle-the-array/my.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        result = []
+                
+        for i in range(0, n):
+            result.append(nums[i])
+            result.append(nums[i + n])
+        
+        return result
