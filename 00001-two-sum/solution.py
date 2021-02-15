e9eb32f906aa2f7f3e1581d8e97c0ebf22d5066d
Eugene Kozlov <kozlovea8@gmail.com>
2021-02-15 16:24:16 +0300

    Update solution.py

--- 00001-two-sum/solution.py
+++ 00001-two-sum/solution.py
@@ -2,11 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         value_index = dict()
         
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            
-            if complement in value_index:
-                return [i, value_index.get(complement)]
+        for i in range(len(nums)):            
+            if target - nums[i] in value_index:
+                return [i, value_index.get(target - nums[i])]
             
             value_index[nums[i]] = i
         
