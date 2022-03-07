3ba680ad9d4f1efc4a8c8625aac84d839277f36d
Eugene Kozlov <kozlovea8@gmail.com>
2022-03-07 13:30:05 +0300

    Update my-optimized.py

--- 00740-delete-and-earn/my-optimized.py
+++ 00740-delete-and-earn/my-optimized.py
@@ -5,14 +5,15 @@ class Solution:
         counter = Counter(nums)
         nums = sorted(set(nums))
     
-        dp = [0] * (len(nums) + 2)
-        
-        dp[2] = nums[0] * counter[nums[0]]
+        dp = [0] * (len(nums) + 1)
+        dp[1] = nums[0] * counter[nums[0]]
     
         for i in range(1, len(nums)):
+            points = nums[i] * counter[nums[i]]
+            
             if nums[i] - nums[i - 1] == 1:
-                dp[i + 2] = max(dp[i + 1], nums[i] * counter[nums[i]] + dp[i])
+                dp[i + 1] = max(dp[i], dp[i - 1] + points)
             else:
-                dp[i + 2] = dp[i + 1] + nums[i] * counter[nums[i]]
+                dp[i + 1] = dp[i] + points
                 
         return dp[-1]
