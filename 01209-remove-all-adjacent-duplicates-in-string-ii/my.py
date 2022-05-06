bccf784e5b7fd2a6da32d660e414ce0a78219093
Eugene Kozlov <kozlovea8@gmail.com>
2022-05-06 13:45:47 +0300

    Update my.py

--- 01209-remove-all-adjacent-duplicates-in-string-ii/my.py
+++ 01209-remove-all-adjacent-duplicates-in-string-ii/my.py
@@ -2,22 +2,13 @@ class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
         buffer = []
         
-        i = 0
-        while(i < len(s)):
-            if not buffer:
-                buffer.append([s[i], 1])
-            elif s[i] != buffer[-1][0]:
-                buffer.append([s[i], 1])
-            else:
+        for c in s:
+            if buffer and c == buffer[-1][0]:
                 buffer[-1][1] += 1
         
                 if buffer[-1][1] == k:
                     buffer.pop()
-            
-            i += 1
-           
-        result = []
-        for c, count in buffer:
-            result.append(c * count)
-        
-        return "".join(result)
+            else:
+                buffer.append([c, 1])
+
+        return "".join([c * count for c, count in buffer])
