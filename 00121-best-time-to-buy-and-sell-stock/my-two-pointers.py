e6fe01b8756ea0167eed3ef634733d04901f3c87
Eugene Kozlov <kozlovea8@gmail.com>
2022-05-22 07:50:06 +0300

    Create my-two-pointers.py

---
+++ 00121-best-time-to-buy-and-sell-stock/my-two-pointers.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        if len(prices) < 2:
+            return 0
+        
+        buy = 0
+        sell = 1
+        
+        while(buy <= sell and sell in range(len(prices))):
+            if buy == sell:
+                sell += 1
+                continue
+            
+            if prices[buy] >= prices[sell]:
+                buy = sell
+                sell = buy + 1
+            else:
+                result = max(result, prices[sell] - prices[buy])
+                sell += 1
+            
+        
+        return result
