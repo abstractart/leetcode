3e489fc84fd99b8838d71efa9adfe34a264a8677
Eugene Kozlov <kozlovea8@gmail.com>
2022-06-19 11:56:41 +0300

    Create my.py

---
+++ 00235-lowest-common-ancestor-bst/my.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        
+        if p.val > q.val:
+            p, q = q, p
+        
+        if root.val == q.val or root.val == p.val:
+            return root
+        
+        if p.val < root.val and q.val > root.val:
+            return root
+        
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
